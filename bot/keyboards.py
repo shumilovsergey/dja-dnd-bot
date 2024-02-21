172844511ea33725b250180f94eb2502a41f0326
Сергей Шумилов <71347401+shumilovsergey@users.noreply.github.com>
2024-02-21 23:02:05 +0300

    Update keyboards.py

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -4,11 +4,15 @@ keyboard_test= {
             {'text': 'D4', 'callback_data': "D4"},
             {'text': 'D6', 'callback_data': "D6"},
             {'text': 'D8', 'callback_data': "D8"},
-            {'text': 'D10', 'callback_data': "D10"},
+            {'text': 'D10', 'callback_data': "D10"}
+            
+        ],
+        [
             {'text': 'D12', 'callback_data': "D12"},
             {'text': 'D16', 'callback_data': "D16"},
             {'text': 'D20', 'callback_data': "D20"},
             {'text': 'del', 'callback_data': "del"}
+            
         ],
     ]
 }
